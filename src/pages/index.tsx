3cace1a2008ec209e4b1e337467e3b9435db2efd
garriagus <agustin_g08@hotmail.com>
2023-05-17 14:09:45 -0300

    MODIFY: fixed bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         {
           <>          
           <PersonalCard >{ }</PersonalCard>
-            <CardList children={undefined} />
+            <CardList  >{""}</CardList>
           </>
         }
       />
